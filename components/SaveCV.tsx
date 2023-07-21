657bdb984b9fe8bd775d9af58bb7a005672bf2c3
Mike Melchior <mike.melchior@outlook.com>
2023-07-21 14:11:44 -0400

    use button component

--- components/SaveCV.tsx
+++ components/SaveCV.tsx
@@ -1,6 +1,7 @@
-import InputContainer from "./InputContainer";
+import InputContainer from "./InputWidget";
 import downloadIcon from "../public/download-icon.png";
 import Image from "next/image";
+import Button from "./Button";
 
 type Props = {};
 
@@ -8,11 +9,11 @@ const SaveCV = (props: Props) => {
 	return (
 		<InputContainer openState={false}>
 			<div className="flex w-full items-center p-[24px]">
-				<h2 className="text-2xl">Save your CV:</h2>
-				<button className="flex items-center gap-4 ml-auto py-[8px] px-[18px] bg-teal-400 border-2 border-teal-400 text-slate-900 rounded-full hover:border-slate-500">
+				<h2 className="text-2xl">Save CV:</h2>
+				<Button>
 					<Image src={downloadIcon} alt="" className="w-[30px]" />
-					DOWNLOAD
-				</button>
+					<p>DOWNLOAD</p>
+				</Button>
 			</div>
 		</InputContainer>
 	);
