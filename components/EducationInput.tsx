0514109588e519db91e401947c2de38abf27d57b
Mike Melchior <mike.melchior@outlook.com>
2023-07-21 14:10:59 -0400

    change sizing/alignment

--- components/EducationInput.tsx
+++ components/EducationInput.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState } from 'react'
-import InputContainer from './InputContainer'
+import InputWidget from './InputWidget'
 import chevron from '../public/chevron.png'
 import gradCap from '../public/grad-cap.png'
 import Image from 'next/image'
@@ -10,16 +10,16 @@ type Props = {}
 const EducationInput = (props: Props) => {
     const [open, setOpen] = useState(false)
     return (
-      <InputContainer openState={open}>
+      <InputWidget openState={open}>
       <div 
-        className={`flex w-full p-[24px] ${open ? 'border-b-2' : ''}`}
+        className={`flex items-center w-full p-[24px] ${open ? 'border-b-2' : ''}`}
         onClick={() => {setOpen(!open)}}
       >
-        <Image src={gradCap} alt="" className='w-[30px] h-auto auto mr-4'/>
+        <Image src={gradCap} alt="" className='w-[30px] h-[30px] auto mr-4'/>
         <h2 className='text-2xl'>General Information</h2>
-        <Image src={chevron} alt="" className={`w-[30px] ml-auto ${open ? 'rotate-180' : 'rotate-0'}`}/>
+        <Image src={chevron} alt="" className={`w-[30px] h-[30px] ml-auto ${open ? 'rotate-180' : 'rotate-0'}`}/>
       </div>
-    </InputContainer> 
+    </InputWidget> 
     )
 }
 
