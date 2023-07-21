37fc4bca6ac526bb80f7b3278d84626cfbc10ae9
Mike Melchior <mike.melchior@outlook.com>
2023-07-21 14:11:14 -0400

    pass state

--- components/Inputs.tsx
+++ components/Inputs.tsx
@@ -1,15 +1,21 @@
+import { Dispatch, SetStateAction } from "react";
 import EducationInput from "./EducationInput";
 import ExperienceInput from "./ExperienceInput";
 import HeaderInput from "./HeaderInput";
 import SaveCV from "./SaveCV";
+import { HeaderInputs } from "@/app/page";
 
-type Props = {};
+type Props = {
+	setGeneralInfo: Dispatch<SetStateAction<HeaderInputs>> | ((prevState: HeaderInputs) => HeaderInputs)
+};
 
-const Inputs = (props: Props) => {
+const Inputs = ( { setGeneralInfo }: Props) => {
 	return (
-		<section className="min-h-[600px] w-full  flex flex-col gap-[24px]">
+		<section className="min-h-[fit-content] max-w-[600px] w-full mx-auto flex flex-col gap-[24px] lg:col-start-2 lg:row-start-1 lg:row-end-3">
 			<SaveCV />
-			<HeaderInput />
+			<HeaderInput 
+				setGeneralInfo={setGeneralInfo}
+			/>
 			<EducationInput />
 			<ExperienceInput />
 		</section>
