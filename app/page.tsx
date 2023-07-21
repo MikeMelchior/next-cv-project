13e6bbd319af9cec6e7d5c65ac3032bf2b3b8e4c
Mike Melchior <mike.melchior@outlook.com>
2023-07-21 14:12:58 -0400

    create interfaces, pass state/setState

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,45 @@
+'use client'
 import Inputs from "@/components/Inputs";
 import Outputs from "@/components/Outputs";
 import Sidebar from "@/components/Sidebar";
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
+
+export interface HeaderInputs{
+	name: string
+	email: string
+	phone: string
+	address: string
+}
+
+export interface EducationInputs{
+
+}
+
+export interface ExperienceInputs{
+	company: string
+	jobTitle: string
+	startDate: string
+	endDate: string
+	description: string
+	id: string
+}
+
+const initialHeaderState = { name: '', email: '', phone: '', address: '' }
 
 export default function Home() {
+
+	const [generalInfo, setGeneralInfo] = useState<HeaderInputs>(initialHeaderState)
+
 	return (
-		<main className="grid md:grid-cols-3 p-[48px] gap-[48px]">
+		<main className="grid lg:grid-cols-2 xl:grid-cols-3 p-[48px] gap-[48px]">
 			<Sidebar />
-      		<Inputs />
-      		<Outputs />
+      		<Inputs 
+				setGeneralInfo={setGeneralInfo}
+			/>
+      		<Outputs 
+				generalInfo={generalInfo}
+			/>
 		</main>
 	);
 }
