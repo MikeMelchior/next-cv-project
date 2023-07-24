ad6539efb70631c624a299b3a06c29dac2355fb0
Mike Melchior <mike.melchior@outlook.com>
2023-07-24 14:50:01 -0400

    add onClick and class params

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { EventHandler, SyntheticEvent } from 'react'
 
 type Props = {
     children: React.ReactNode
     type?: "button" | "submit" | "reset" | undefined
+    onClick?: EventHandler<SyntheticEvent>
+    classes?: string
 }
 
-const Button = ( { children, type="button" }: Props) => {
+const Button = ( { children, type="button", onClick, classes }: Props) => {
   return (
     <button
-    className="flex items-center justify-center min-w-fit gap-4 ml-auto py-[8px] px-[18px] text-base bg-teal-200 border-2 border-teal-400 text-slate-900 rounded-full hover:bg-teal-400"
-    type={type}
+		className={`flex flex-1 items-center justify-center min-w-fit gap-4 py-[8px] px-[18px] text-base bg-teal-200 border-2 border-teal-400 text-slate-900 rounded-full hover:bg-teal-400" ${classes}`}
+		type={type}
+		onClick={onClick}
     >
         {children}
     </button>
