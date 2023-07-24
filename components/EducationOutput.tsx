b6ff8f3d798db54d6c6a7b7c8d3c27d4cb3dc664
Mike Melchior <mike.melchior@outlook.com>
2023-07-24 14:52:52 -0400

    format

--- components/EducationOutput.tsx
+++ components/EducationOutput.tsx
@@ -1,28 +1,41 @@
-import { EducationInputs } from '@/app/page'
-import React from 'react'
+import React from "react";
+import { EducationInputs } from "@/app/page";
+import { getMonth, getYear } from "@/utils/dateFunctions";
 
 type Props = {
-    educationInfo: EducationInputs[]
-}
+	educationInfo: EducationInputs[];
+};
 
-const EducationOutput = ( { educationInfo }: Props) => {
-  return (
-    <div className='w-full border-t-2 pt-6'>
-        {educationInfo && 
-            educationInfo.map(edu => (
-                <div 
-                    key={edu.id}
-                    className=''
-                >
-                    <h1>{edu.degree}</h1>
-                    <p>{edu.school}</p>
-                    <p>{`${edu.city}, ${edu.country}`}</p>
-                    <p>{`${edu.startDate} - ${edu.endDate}`}</p>
-                </div>    
-            ))
-        }
-    </div>
-  )
-}
+const EducationOutput = ({ educationInfo }: Props) => {
 
-export default EducationOutput
+	return (
+		<div className={`w-full pt-6 ${educationInfo.length > 0 && "border-t-2"}`}>
+			{educationInfo.length > 0 &&
+				educationInfo.map((edu) => (
+					<div key={edu.id} className="flex justify-between text-sm">
+						<div className="text-left">
+							<p className="font-bold text-base">{edu.degree}</p>
+							<p><em>{edu.school}</em></p>
+							
+						</div>
+
+                        <div className="text-right">
+                            <p className="text-base">
+                                {`
+									${getMonth(edu.startDate)} ${getYear(edu.startDate)} - 
+                                    ${edu.endDate !== 'Current' 
+                                        ? `${getMonth(edu.endDate)} ${getYear(edu.endDate)}` 
+                                        : 'Current'
+                                    }
+                                `}
+                            </p>
+                            <p>{`${edu.city}, ${edu.country}`}</p>
+                        </div>
+
+					</div>
+				))}
+		</div>
+	);
+};
+
+export default EducationOutput;
