ed673cf3c9f915f3789b3d60685500903f6bd4fb
Mike Melchior <mike.melchior@outlook.com>
2023-07-25 09:45:06 -0400

    bold font

--- components/HeaderOutput.tsx
+++ components/HeaderOutput.tsx
@@ -8,7 +8,7 @@ type Props = {
 const HeaderOutput = ({ generalInfo }: Props) => {
 	return (
 		<div className="w-full flex flex-col gap-1 pb-6">
-			<p className="text-3xl">{generalInfo.name}</p>
+			<p className="text-3xl font-bold">{generalInfo.name}</p>
 			<p className="text-base">{generalInfo.email}</p>
 			<div className="text-sm flex gap-4 mx-auto">
 				<p>{generalInfo.phone}</p>
