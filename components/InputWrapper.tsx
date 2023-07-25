4c682a1af5c0e77dfcd97c564091836a738fefe6
Mike Melchior <mike.melchior@outlook.com>
2023-07-25 09:45:28 -0400

    add classes param

--- components/InputWrapper.tsx
+++ components/InputWrapper.tsx
@@ -4,11 +4,12 @@ type Props = {
   children: React.ReactNode
 	HTMLfor: string;
 	labelText: string;
+	classes?: string
 };
 
-const InputWrapper = ({ children, HTMLfor, labelText }: Props) => {
+const InputWrapper = ({ children, HTMLfor, labelText, classes }: Props) => {
 	return (
-		<label htmlFor={HTMLfor} className="flex flex-col">
+		<label htmlFor={HTMLfor} className={`flex flex-col ${classes ? classes : ''}`}>
 			<p className="ml-2 mb-2 font-semibold">{labelText}</p>
         	{children}
 		</label>
