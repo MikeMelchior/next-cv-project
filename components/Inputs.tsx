20aad6e85400b88cbb5058d14881f1360e8951b5
Mike Melchior <mike.melchior@outlook.com>
2023-07-25 09:45:17 -0400

    add/pass props

--- components/Inputs.tsx
+++ components/Inputs.tsx
@@ -3,15 +3,17 @@ import EducationInput from "./EducationInput";
 import ExperienceInput from "./ExperienceInput";
 import HeaderInput from "./HeaderInput";
 import SaveCV from "./SaveCV";
-import { EducationInputs, HeaderInputs } from "@/app/page";
+import { EducationInputs, ExperienceInputs, HeaderInputs } from "@/app/page";
 
 type Props = {
 	setGeneralInfo: Dispatch<SetStateAction<HeaderInputs>> | ((prevState: HeaderInputs) => HeaderInputs)
 	educationInfo: EducationInputs[]
 	setEducationInfo: Dispatch<SetStateAction<EducationInputs[]>> | ((prevState: EducationInputs[]) => EducationInputs[])
+	experienceInfo: ExperienceInputs[]
+	setExperienceInfo: Dispatch<SetStateAction<ExperienceInputs[]>> | ((prevState: ExperienceInputs[]) => ExperienceInputs[])
 };
 
-const Inputs = ( { setGeneralInfo, educationInfo, setEducationInfo }: Props) => {
+const Inputs = ( { setGeneralInfo, educationInfo, setEducationInfo, experienceInfo, setExperienceInfo }: Props) => {
 	return (
 		<section className="min-h-[fit-content] max-w-[600px] w-full mx-auto flex flex-col gap-8 lg:col-start-2 lg:row-start-1 lg:row-end-3">
 			<SaveCV />
@@ -22,7 +24,10 @@ const Inputs = ( { setGeneralInfo, educationInfo, setEducationInfo }: Props) => 
 				educationInfo={educationInfo}
 				setEducationInfo={setEducationInfo}
 			/>
-			<ExperienceInput />
+			<ExperienceInput 
+				experienceInfo={experienceInfo}
+				setExperienceInfo={setExperienceInfo}
+			/>
 		</section>
 	);
 };
