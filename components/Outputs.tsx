7eb13fe684837cb7d2e9a221a8f8ded929af03ca
Mike Melchior <mike.melchior@outlook.com>
2023-07-25 09:45:47 -0400

    add and pass props to new component

--- components/Outputs.tsx
+++ components/Outputs.tsx
@@ -1,17 +1,20 @@
-import { EducationInputs, HeaderInputs } from "@/app/page";
+import { EducationInputs, ExperienceInputs, HeaderInputs } from "@/app/page";
 import { Quicksand } from "next/font/google";
 import React from "react";
 import HeaderOutput from "./HeaderOutput";
 import EducationOutput from "./EducationOutput";
+import ExperienceOutput from "./ExperienceOutput";
+
 
 const quicksand = Quicksand({ subsets: ['latin']})
 
 type Props = {
 	generalInfo: HeaderInputs
 	educationInfo: EducationInputs[]
+	experienceInfo: ExperienceInputs[]
 };
 
-const Outputs = ( { generalInfo, educationInfo }: Props) => {
+const Outputs = ( { generalInfo, educationInfo, experienceInfo }: Props) => {
 	return (
 		<div className={`document ${quicksand.className} h-[600px] min-h-[80vh] min-w-[300px] p-[48px] bg-slate-50 text-center rounded-lg flex flex-col`}>
 			<HeaderOutput 
@@ -19,6 +22,9 @@ const Outputs = ( { generalInfo, educationInfo }: Props) => {
 			/>
 			<EducationOutput 
 				educationInfo={educationInfo}
+			/>
+			<ExperienceOutput 
+				experienceInfo={experienceInfo}
 			/>
 		</div>
 	);
